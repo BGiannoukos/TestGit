9a01884c797ae76c11dc4df49160adbac9e22faa
Bill <vgiannoukos7@gmail.com>
2024-10-12 23:57:00 +0300

    dasdas

--- src/com/bill/Test.java
+++ src/com/bill/Test.java
@@ -4,8 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
            
+<<<<<<< Updated upstream
 
 System.out.println("playerYolo13");
+=======
+System.out.println("playerYolo19");
+>>>>>>> Stashed changes
 
 	}
 
