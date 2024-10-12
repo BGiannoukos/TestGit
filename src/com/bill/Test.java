aef6f149b90912217947c5b38432040dd82b7e6e
BGiannoukos <52173327+BGiannoukos@users.noreply.github.com>
2024-10-12 23:58:35 +0300

    Update Test.java

--- src/com/bill/Test.java
+++ src/com/bill/Test.java
@@ -4,12 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
            
-<<<<<<< Updated upstream
+System.out.println("playerYolo99");
 
-System.out.println("playerYolo13");
-=======
-System.out.println("playerYolo19");
->>>>>>> Stashed changes
 
 	}
 
