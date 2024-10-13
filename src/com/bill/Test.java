e911c7f7daaa1d652e3934e2adb74a5a49d6183e
Bill <vgiannoukos7@gmail.com>
2024-10-13 13:08:34 +0300

    fdsffsd

--- src/com/bill/Test.java
+++ src/com/bill/Test.java
@@ -4,7 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
            
+<<<<<<< Updated upstream
 System.out.println("playerYolo7");
+=======
+
+System.out.println("playerYolo2");
+>>>>>>> Stashed changes
 
 	}
 
