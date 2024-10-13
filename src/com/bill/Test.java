2d7b46944393d668e701ca37dc4e4d875e3db3a1
Bill <vgiannoukos7@gmail.com>
2024-10-13 13:25:11 +0300

    adsad

--- src/com/bill/Test.java
+++ src/com/bill/Test.java
@@ -4,18 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
            
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-System.out.println("playerYolo7");
-=======
-
-System.out.println("playerYolo2");
->>>>>>> Stashed changes
-=======
-
-System.out.println("playerYolo2");
->>>>>>> Stashed changes
-
+System.out.println("damn");
 	}
 
 
